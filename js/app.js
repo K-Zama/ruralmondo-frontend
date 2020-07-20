d43e9a180ad28cdef4521812bc2764026f382ac2
K-Zama <zamarreno.plaza@gmail.com>
2020-07-20 17:56:25 +0200

    La verdad es que lo he intentado

--- js/app.js
+++ js/app.js
@@ -12,8 +12,10 @@
 // Variables
 const playerList = document.querySelector("#playerList");
 const players = document.querySelectorAll("#player");
-const playerPrice = document.querySelector("#playerPrice");
+const playerPrice = document.querySelectorAll("#playerPrice");
 const btnPlayers = document.querySelector("#btn-players");
+let costBudget;
+
 
 // JSON de prueba
 class Player {
@@ -27,11 +29,16 @@ const camacho = new Player("Camacho", 5);
 
 const josefe = new Player("Josefe", 2);
 
+
 // Pintar en los huecos en el HTML
 players[0].innerHTML = `${camacho.name} <span class="badge badge-primary badge-pill" id="playerPrice">${camacho.cost}€</span>`;
 players[1].innerHTML = `${josefe.name} <span class="badge badge-primary badge-pill" id="playerPrice">${josefe.cost}€</span>`;
 const costPlayersArray = [camacho.cost, josefe.cost];
 // Añadir y restar presupuesto
+const budgetUser = document.querySelector("#presupuesto");
+budgetUser.innerHTML = `${9}€`
+
+const budget = budgetUser.value
 
 
 // Cambiar clase active
